b6a2351b1a12abffe1ea9a0d8df35136c8b6a52f
Manish kumar <manishmandal9734@gmail.com>
2024-08-10 20:03:57 +0530

    commit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,7 +13,7 @@ function Login() {
 
   const handleGoogleLogin = () => {
     // Redirect to Google login URL
-    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=http://localhost:5173/";
+    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reach-inbox-ai-three.vercel.app/";
   };
 
   return (
